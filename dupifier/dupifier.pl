e026a9cd285e180b2646613278fbc7f68567f2b4
ben <ben@e34c58d7-84fb-0310-b8da-d1fa509e4264>
2005-07-10 05:40:35 +0000

    Add dupifier
    git-svn-id: http://svn.bleything.net/toys@2 e34c58d7-84fb-0310-b8da-d1fa509e4264

---
+++ dupifier/dupifier.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Find;
+use Digest::MD5 qw(md5_hex);
+
+die("Usage: $0 <absolute path to Maildir>\n") unless $ARGV[0];
+
+my %hash;
+my ( $counter, $dupes );
+
+$counter = 0;
+
+sub process {
+    my $file = $File::Find::name;
+
+    return unless $file =~ m!/(cur|new|tmp)/.!;
+
+    open FILE, "< $file" or die "Couldn't open file $file: $!";
+    $counter++;
+
+    my ( $body, $inheader ) = ( "", 1 );
+    while (<FILE>) {
+        $inheader = 0 if /^$/;
+
+        unless ($inheader) {
+            $body .= $_;
+        }
+    }
+    close FILE;
+
+    $body =~ s/\s|\n//g;
+
+    my $digest = md5_hex($body);
+
+    push @{ $hash{$digest} }, $file;
+}
+
+find( \&process, shift );
+
+foreach ( keys %hash ) {
+    next unless scalar( @{ $hash{$_} } ) > 1;
+
+    $dupes++;
+
+    $, = "\n";
+    print @{ $hash{$_} };
+    print "\n\n";
+}
+
+print "DONE: processed $counter messages, ";
+print "found $dupes sets of messages that may be dupes.\n";
