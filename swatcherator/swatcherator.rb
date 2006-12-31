3ca8dc7b0855d9cdaf8c97046e7916d5dddaaf2b
ben <ben@e34c58d7-84fb-0310-b8da-d1fa509e4264>
2006-12-31 04:14:34 +0000

    source:/swatcherator/swatcherator.rb  * Refactor color definitions into a hash of hashes containing the following keys:    * name: the english name of the color (example: Burnt Orange / burnt_orange)    * hexcode: the 6-character hex code for the color, without the leading #    * class: whether the color is light or dark. Controls the color of the text in the swatch    * enabled: whether or not this color should be included in the rotation  * updated views to reflect above changes  * Colors are stored in a constant (COLORS) which is now duplicated into the session and used in place of COLORS around the code  * @swatches only includes enabled colors  * redid stylesheet to be a cleaner design (inspired by flexilist)  * refactored layout to match the new stylesheet
    git-svn-id: http://svn.bleything.net/toys@31 e34c58d7-84fb-0310-b8da-d1fa509e4264

--- swatcherator/swatcherator.rb
+++ swatcherator/swatcherator.rb
@@ -8,21 +8,188 @@ require 'camping/session'
 Camping.goes :Swatcherator
 
 COLORS = {
-  :baby_blue    => '90c0d6', :baby_pink   => 'ffb2b6', :berry        => '942869',
-  :black        => '0f0f0f', :brown       => '634228', :burnt_orange => 'cf4206',
-  :chartreuse   => 'c4da39', :coral       => 'fc574e', :dark_green   => '264014',
-  :dijon        => 'c97d02', :gold        => 'ffba30', :gray         => '4e5251',
-  :leaf_green   => '437709', :light_green => '829c13', :mocha_brown  => 'ab875b',
-  :navy         => '0e1e26', :olive       => '4f441d', :orange       => 'f26500',
-  :peacock_blue => '028587', :purple      => '6b2e85', :red          => 'cc042f',
-  :silver       => 'adb0ab', :slate_blue  => '136485', :soft_yellow  => 'fcd75d',
-  :strawberry   => 'c90c19', :white       => 'f7f7f7'
-}
+  :baby_blue => {
+    :name    => 'Baby Blue',
+    :hexcode => '90c0d6',
+    :class   => 'light',
+    :enabled => true,
+  },
 
-DARKS = [
-  :berry, :black, :brown,        :burnt_orange, :dark_green, :gray,       :leaf_green, 
-  :navy,  :olive, :peacock_blue, :purple,       :red,        :slate_blue, :strawberry
-]
+  :baby_pink => {
+    :name    => 'Baby Pink',
+    :hexcode => 'ffb2b6',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :berry => {
+    :name    => 'Berry',
+    :hexcode => '942869',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :black => {
+    :name    => 'Black',
+    :hexcode => '0f0f0f',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :brown => {
+    :name    => 'Brown',
+    :hexcode => '634228',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :burnt_orange => {
+    :name    => 'Burnt Orange',
+    :hexcode => 'cf4206',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :chartreuse => {
+    :name    => 'Chartreuse',
+    :hexcode => 'c4da39',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :coral => {
+    :name    => 'Coral',
+    :hexcode => 'fc574e',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :dark_green => {
+    :name    => 'Dark Green',
+    :hexcode => '264014',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :dijon => {
+    :name    => 'Dijon',
+    :hexcode => 'c97d02',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :gold => {
+    :name    => 'Gold',
+    :hexcode => 'ffba30',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :gray => {
+    :name    => 'Gray',
+    :hexcode => '4e5251',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :leaf_green => {
+    :name    => 'Leaf Green',
+    :hexcode => '437709',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :light_green => {
+    :name    => 'Light Green',
+    :hexcode => '829c13',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :mocha_brown => {
+    :name    => 'Mocha Brown',
+    :hexcode => 'ab875b',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :navy => {
+    :name    => 'Navy',
+    :hexcode => '0e1e26',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :olive => {
+    :name    => 'Olive',
+    :hexcode => '4f441d',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :orange => {
+    :name    => 'Orange',
+    :hexcode => 'f26500',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :peacock_blue => {
+    :name    => 'Peacock Blue',
+    :hexcode => '028587',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :purple => {
+    :name    => 'Purple',
+    :hexcode => '6b2e85',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :red => {
+    :name    => 'Red',
+    :hexcode => 'cc042f',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :silver => {
+    :name    => 'Silver',
+    :hexcode => 'adb0ab',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :slate_blue => {
+    :name    => 'Slate Blue',
+    :hexcode => '136485',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :soft_yellow => {
+    :name    => 'Soft Yellow',
+    :hexcode => 'fcd75d',
+    :class   => 'light',
+    :enabled => true,
+  },
+
+  :strawberry => {
+    :name    => 'Strawberry',
+    :hexcode => 'c90c19',
+    :class   => 'dark',
+    :enabled => true,
+  },
+
+  :white => {
+    :name    => 'White',
+    :hexcode => 'f7f7f7',
+    :class   => 'light',
+    :enabled => true,
+  },  
+}
 
 NUMBER_OF_SWATCHES = 3
 
@@ -31,10 +198,9 @@ module Swatcherator ; include Camping::Session ; end
 module Swatcherator::Controllers
   class Index < R '/'
     def get
-      @swatches = COLORS.keys.sort_by{rand}[0..(NUMBER_OF_SWATCHES - 1)]
+      @state[:colors] ||= COLORS
       
-      @state[:enabled_colors] ||= COLORS.keys
-      
+      @swatches = @state[:colors].select {|c,v| v[:enabled]}.sort_by {rand}[0..(NUMBER_OF_SWATCHES - 1)]
       render :index
     end
   end
@@ -48,13 +214,40 @@ module Swatcherator::Views
         style do
           <<-END
           body {
-            background: #333333;
-            
+            margin: 0;
+            font-family: sans-serif;
+            background: #444444;
             color: silver;
           }
-          
-          h1 {
+
+          #footer, #header {
+            background: #333333;
+            color: #cccccc;
             text-align: center;
+            width: 100%;
+          }
+
+          #header {
+            font-size: 2em;
+            margin-bottom: 2em;
+            padding: 0.3em 0 0.15em 0;
+          }
+
+          #footer {
+            padding: 0.5em 0 0.2em 0;
+            font-size: 0.8em;
+            margin-top: 2em;
+            position: absolute;
+            bottom: 0;
+          }
+
+          #footer a, #header a {
+            color: #d88722;
+          }
+          
+          #container {
+            margin: auto;
+            width: 95%;
           }
           
           #selector {
@@ -95,9 +288,9 @@ module Swatcherator::Views
             position: relative;
             top: 45%;
           }
-
-          span {color:black}
-          span.light {color: white}
+          
+          span.light {color:black}
+          span.dark  {color: white}
           
           .clear { clear:both; }
           END
@@ -105,11 +298,15 @@ module Swatcherator::Views
       end
       
       body do
-        h1 "Swatcherator"
+        div.header! "Swatcherator"
 
-        _color_selector
+        div.container! do
+          _color_selector
 
-        self << yield
+          self << yield
+        end
+        
+        div.footer! "Swatcherator Copyright 2007 Ben Bleything.  All Rights Reserved.  Color Schemes are not included under this copyright."
       end
     end
   end
@@ -122,12 +319,13 @@ module Swatcherator::Views
   
   def _color_selector
     div.selector! do
-      COLORS.keys.map(&:to_s).sort.each do |color|
-        color = color.intern
+      # sort colors in alphabetical order.  Probably a better way.  I'm tired.
+      @state[:colors].keys.map(&:to_s).sort.map(&:intern).each do |code|
+        color = @state[:colors][code]
 
-        div.colorbox :style => "background: ##{COLORS[color]}" do
-          opts = {:type => :checkbox, :name => color}
-          opts.merge! :checked => true if @state[:enabled_colors].include? color
+        div.colorbox :style => "background: ##{color[:hexcode]}" do
+          opts = {:type => :checkbox, :name => code}
+          opts.merge! :checked => true if color[:enabled]
           
           input opts
         end
@@ -139,11 +337,9 @@ module Swatcherator::Views
   
   def _swatches
     div.swatches! do
-      @swatches.each do |color|
-        div.colorbox :style => "background: ##{COLORS[color]}" do
-          color_name = color.to_s.split('_').map(&:capitalize).join(" ")
-
-          span color_name, :class => (DARKS.include? color) ? 'light' : nil
+      @swatches.each do |code, color|
+        div.colorbox :style => "background: ##{color[:hexcode]}" do
+          span color[:name], :class => color[:class]
         end
       end
     end
