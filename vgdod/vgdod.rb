95f9a9ed6daa787dd0eb3929f23a952824eab75e
ben <ben@e34c58d7-84fb-0310-b8da-d1fa509e4264>
2007-12-14 07:53:15 +0000

    * update for new amazon setup, remove ShortURL stuff
    git-svn-id: http://svn.bleything.net/toys@51 e34c58d7-84fb-0310-b8da-d1fa509e4264

--- vgdod/vgdod.rb
+++ vgdod/vgdod.rb
@@ -1,16 +1,16 @@
-#!/usr/bin/env ruby
+#!/opt/local/bin/ruby
 
-%w(rubygems hpricot open-uri shorturl).each {|g| require g}
+%w(rubygems hpricot open-uri).each {|g| require g}
 def fetch(url = '') ; return Hpricot(open("http://www.amazon.com" + url)) ; end
 
 vg_url = fetch.at("a[text()=Video Games]")[:href]
-dotd_img = fetch(vg_url).search("img").select {|e| e[:src] =~ /deal-of-the-day/}.first
-dotd = fetch(dotd_img.parent[:href])
+dotd_map = fetch(vg_url).search("map").select {|e| e[:name] =~ /dotd/}.first
+dotd = fetch(dotd_map.at("area")[:href])
+# dotd = fetch("/Microsoft-9UE-00001-Halo-3/dp/B000FRU0NU/ref=pd_bbs_sr_1/103-5342003-6915053?ie=UTF8&s=videogames&qid=1194816939&sr=8-1")
 
 # title -- platform -- price -- url
 puts [
   dotd.at("//div.buying//b.sans").inner_text.strip,
-  dotd.search("//b.price").last.inner_text.strip,
+  dotd.search("//td/b.price").last.inner_text.strip,
   dotd.search("//div[@class=buying]")[3].inner_text.gsub(/(\&nbsp;|Platform:)/, '').strip,
-  WWW::ShortURL.shorten("http://www.amazon.com#{dotd_img.parent[:href]}")
 ].join( " -- " )
