59c482a5336c3f07aeafb5cc44d0aa69c986e524
ben <ben@e34c58d7-84fb-0310-b8da-d1fa509e4264>
2008-04-24 17:44:53 +0000

    * update for new amazon vgdod html
    git-svn-id: http://svn.bleything.net/toys@53 e34c58d7-84fb-0310-b8da-d1fa509e4264

--- vgdod/vgdod.rb
+++ vgdod/vgdod.rb
@@ -4,13 +4,19 @@
 def fetch(url = '') ; return Hpricot(open("http://www.amazon.com" + url)) ; end
 
 vg_url = fetch.at("a[text()=Video Games]")[:href]
+puts " * found video games URL: #{vg_url}" if $DEBUG
+
 dotd_img = fetch(vg_url).search("img").select {|e| e[:src] =~ /deal-of-the-day/}.first
+puts " * found dotd image href: #{dotd_img.parent[:href]}" if $DEBUG
+
 dotd = fetch(dotd_img.parent[:href])
 
-# title -- platform -- price -- url
-puts [
-  dotd.at("//div.buying//b.sans").inner_text.strip,
-  dotd.search("//b.price").last.inner_text.strip,
-  dotd.search("//div[@class=buying]")[3].inner_text.gsub(/(\&nbsp;|Platform:)/, '').strip,
-  ShortURL.shorten("http://www.amazon.com#{dotd_img.parent[:href]}")
-].join( " -- " )
+
+table = dotd.at( "//table.amabot_widget" )
+
+
+puts "Title:      " + table.at( "/tr/td[3]/a" ).inner_text.strip
+puts "Platform:   " + table.at( "/tr[2]/td" ).inner_text.match( /Platform: ..(.*?)Video Game/ )[1]
+puts "List Price: " + table.at( "//span[@class=listprice]" ).inner_text.strip
+puts "Sale Price: " + table.at( "//b[@class=price]" ).inner_text.strip
+puts "Link:       " + ShortURL.shorten( "http://www.amazon.com" + table.at( "/tr/td[3]/a" )[:href] )
