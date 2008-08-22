e97e3a2ca98cdf5dbee64230e7e72ae2dea1ea34
Ben Bleything <ben@bleything.net>
2008-08-22 01:21:45 -0700

    add extraction phase to analyzer

--- mailfixer/analyzer.rb
+++ mailfixer/analyzer.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/env ruby
 
+##############################################################################
+### U T I L I T Y   F U N C T I O N S
+##############################################################################
+
+def log( msg = "" )
+	$stderr.puts msg
+end
+
+$stdout.sync = true
+
+##############################################################################
+### D A T A B A S E   S E T U P
+##############################################################################
+
 require 'rubygems'
 require 'sequel'
 
@@ -16,3 +30,48 @@ DB.create_table :edges do
 	varchar :edge_type,  :index => true
 	int     :address_id, :index => true
 end unless DB.table_exists?( :edges )
+
+@mail_details = DB.from( :mail_details )
+@addresses    = DB.from( :addresses    )
+@edges        = DB.from( :edges        )
+
+##############################################################################
+### E X T R A C T I O N   P H A S E
+##############################################################################
+
+require 'tmail'
+
+log "Extracting details from %i messages" % 
+	[ @mail_details.filter( :extracted => false ).size ]
+
+count = 0
+@mail_details.filter( :extracted => false ).each do |msg|
+	mail = TMail::Mail.load( msg[:path] )
+	
+	to = mail.to.select {|addr| addr =~ /bleything.net/ }.join( "," ) rescue ""
+	cc = mail.cc.select {|addr| addr =~ /bleything.net/ }.join( "," ) rescue ""
+	
+	deliveredto   = mail.header[ 'delivered-to'    ].to_s
+	xapparentlyto = mail.header[ "x-apparently-to" ].to_s
+	
+	deliveredto   = '' unless deliveredto   =~ /bleything.net/
+	xapparentlyto = '' unless xapparentlyto =~ /bleything.net/
+	
+	@mail_details.filter( :path => msg[:path] ).update(
+		:toheader      => to,
+		:cc            => cc,
+		:deliveredto   => deliveredto,
+		:xapparentlyto => xapparentlyto,
+		:extracted     => true
+	)
+	
+	count += 1
+	
+	if count % 10 == 0
+		print count
+	else
+		print '.'
+	end
+end
+
+log "...done!"
