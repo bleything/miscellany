39c611e1e1a0398006ed5f45643f69b8dc95d81a
Ben Bleything <ben@bleything.net>
2008-08-21 22:37:29 -0700

    add analyzer.rb skeleton

---
+++ mailfixer/analyzer.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'sequel'
+
+# Sequel database object
+DB = Sequel.connect( 'postgres://localhost' )
+
+DB.create_table :addresses do
+	serial :id
+	varchar :address, :index => true
+end.unless DB.table_exists? :addresses
+
+DB.create_table :edges do
+	varchar :path,       :index => true
+	varchar :edge_type,  :index => true
+	int     :address_id, :index => true
+end.unless DB.table_exists? :edges
