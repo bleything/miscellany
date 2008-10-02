13c019776629ac7ea52e4d94c4615ce8200466b4
Ben Bleything <ben@bleything.net>
2008-10-02 11:19:52 -0700

    update vgdod.rb for various html changes at amazon

--- vgdod/vgdod.rb
+++ vgdod/vgdod.rb
@@ -11,12 +11,14 @@ puts " * found dotd image href: #{dotd_img.parent[:href]}" if $DEBUG
 
 dotd = fetch(dotd_img.parent[:href])
 
-
 table = dotd.at( "//table.amabot_widget" )
 
-
 puts "Title:      " + table.at( "/tr/td[3]/a" ).inner_text.strip
-puts "Platform:   " + table.at( "/tr[2]/td" ).inner_text.match( /Platform: ..(.*?)Video Game/ )[1]
+
+puts "Platform:   " + table.at( "/tr[2]/td" ).inner_html.
+	match( /Platform:.*?\/>&nbsp;(.*?)$/ )[1].
+	gsub( /<\/?b>/, '' ).gsub( "<br />", ' ' ) # strip out <b> and <br /> tags
+
 puts "List Price: " + table.at( "//span[@class=listprice]" ).inner_text.strip
 puts "Sale Price: " + table.at( "//b[@class=price]" ).inner_text.strip
 puts "Link:       " + ShortURL.shorten( "http://www.amazon.com" + table.at( "/tr/td[3]/a" )[:href] )
