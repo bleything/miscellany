41c1c8da2a73cbb6e253d3307333a9ead9ac7a84
Ben Bleything <ben@bleything.net>
2009-01-25 22:49:19 -0800

    add md5 and sha1 analyzers, although I'm probably going to consolidate them here in like 5 minutes

---
+++ mp3duper/md5_analyzer.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby -w
+
+### Discovers all files whose md5 hashes are the same
+
+require 'rubygems'
+require 'sequel'
+
+DB = Sequel.connect('postgres://localhost')
+
+duplicate_md5s = DB[ 
+  "SELECT md5                " +
+  "FROM mp3s                 " +
+  "WHERE md5 IS NOT NULL AND " +
+  "actuallymusic IS true     " +
+  "GROUP BY md5              " +
+  "HAVING COUNT(path) > 1    " +
+  "ORDER BY COUNT(path) DESC;"
+]
+
+duplicate_md5s.each do |dupe|
+  duplicate_files = DB.from( :mp3s ).
+    filter( :md5 => dupe[:md5], :actuallymusic => true ).
+    map( :path )
+  
+  puts dupe[:md5]
+  puts "=" * dupe[:md5].length
+
+  puts duplicate_files.join( "\n" )
+
+  puts
+end
---
+++ mp3duper/sha1_analyzer.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby -w
+
+### Discovers all files whose sha1 hashes are the same
+
+require 'rubygems'
+require 'sequel'
+
+DB = Sequel.connect('postgres://localhost')
+
+duplicate_sha1s = DB[ 
+  "SELECT sha1                " +
+  "FROM mp3s                  " +
+  "WHERE sha1 IS NOT NULL AND " +
+  "actuallymusic IS true      " +
+  "GROUP BY sha1              " +
+  "HAVING COUNT(path) > 1     " +
+  "ORDER BY COUNT(path) DESC; "
+]
+
+duplicate_sha1s.each do |dupe|
+  duplicate_files = DB.from( :mp3s ).
+    filter( :sha1 => dupe[:sha1], :actuallymusic => true ).
+    map( :path )
+  
+  puts dupe[:sha1]
+  puts "=" * dupe[:sha1].length
+
+  puts duplicate_files.join( "\n" )
+
+  puts
+end
