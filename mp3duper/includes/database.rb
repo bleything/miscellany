ad896f6376ea46a9a010d70d69f95d35b75745e1
Ben Bleything <ben@bleything.net>
2009-08-31 20:38:02 -0700

    make includes/database.rb install the right schema

--- mp3duper/includes/database.rb
+++ mp3duper/includes/database.rb
@@ -13,17 +13,22 @@ require 'sequel'
 DB = Sequel.connect( CONFIG['db_uri'] )
 
 DB.create_table :mp3s do
+  primary_key :id
+
   text :path, :unique => true
   int  :size
-  
+
   # fingerprints
   varchar :puid, :index => true
   varchar :sha1, :index => true
   varchar :md5,  :index => true
-  
+
   # status markers
   boolean :fingerprinted, :index => true, :default => false
   boolean :hashed,        :index => true, :default => false
+
+  # miscellaneous flags
+  boolean :actuallymusic, :index => true, :default => false
 end unless DB.table_exists?( :mp3s )
 
 @mp3s = DB.from( :mp3s )
