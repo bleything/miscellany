62d799746fe5e9f4bfd5ce8396b9611b13c3f1e4
Ben Bleything <ben@bleything.net>
2009-10-09 13:19:48 -0700

    check in the fingerprinter and associated analyzer that have just been laying around forever

---
+++ mp3duper/fingerprint_analyzer.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'includes/database'
+
+### Discovers all files whose acoustic fingerprints are the same
+duplicate_puids = DB[
+  "SELECT puid                " +
+  "FROM mp3s                  " +
+  "WHERE puid IS NOT NULL AND " +
+  "actuallymusic IS true      " +
+  "GROUP BY puid              " +
+  "HAVING COUNT(path) > 1     " +
+  "ORDER BY COUNT(path) DESC; "
+]
+
+duplicate_puids.each do |dupe|
+  duplicate_files = DB.from( :mp3s ).
+    filter( :puid => dupe[ :puid ], :actuallymusic => true )
+
+  next unless duplicate_files.count > 1
+
+  puts dupe[:puid]
+  puts "=" * dupe[:puid].length
+
+  duplicate_files.each do |dupe|
+    puts "#{dupe[:size]} -- #{dupe[:path]}"
+  end
+
+  puts
+end
---
+++ mp3duper/fingerprinter.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+
+require 'includes/config'
+require 'includes/database'
+require 'includes/helpers'
+
+##############################################################################
+### F I N D   F I L E S   I N   D A T A B A S E
+##############################################################################
+
+log "Fetching files from the database..."
+db_files = @mp3s.
+  filter(  :fingerprinted => false, :actuallymusic => nil ).map( :path )
+
+log "...done! Found #{@mp3s.count} files, of which #{db_files.size} need to be fingerprinted."
+
+log # blank line
+
+##############################################################################
+### S A N I T Y   C H E C K S
+##############################################################################
+
+unless File.exists? 'genpuid/genpuid'
+  log "Can't find genpuid executable.  Make sure there's a directory called genpuid with a genpuid executable inside it."
+  exit 1
+end
+
+##############################################################################
+### F I N G E R P R I N T   T I E M ! ! ! 1
+##############################################################################
+print "Finding PUIDs for #{db_files.size} files..."
+
+count = 0
+
+db_files.each do |path|
+  output = `genpuid/genpuid #{CONFIG['musicdns_key']} "#{path}"`
+  
+  if output =~ /Incorrect MusicDNS key/
+    log "MusicDNS rejected your key.  Please check their site and update your config.yml."
+    exit 1
+  end
+  
+  # mark the file as seen so we'll skip it next time even if there's no
+  # PUID.  This prevents us from cranking over the same broken files every
+  # time we start up
+  @mp3s.filter( :path => path ).update( :fingerprinted => true )
+  
+  # if puid
+  if puid = output.match( /puid: (.*)$/ )[1] rescue nil
+    # update with the new PUID
+    @mp3s.filter( :path => path ).update( :puid => puid )
+    symbol = '+'
+  else
+    # if we don't get a PUID out of genpuid, let's just move along
+    symbol = '.'
+  end
+  
+  # status display
+  count += 1
+  
+  if ( count % 10 ) == 0
+    print count
+  else
+    print symbol
+  end
+end
+
+log "...done!"
+
